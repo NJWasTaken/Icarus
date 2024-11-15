ae7c572e362742b2ab95cd7fe45b91ccf928766f
NJWasTaken <114809171+NJWasTaken@users.noreply.github.com>
2024-11-15 21:22:14 +0530

    backend structure done

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from 'mongoose';
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Mongo also finna connected: ${conn.connection.host}`);
+        console.log(`Mongo is cooking something up at: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error lmao: ${error.message}`);
         process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -2,50 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Expense from "./models/expenses.model.js";
+import mongoose from "mongoose";
+
+import expenseRoutes from "./routes/expense.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json()); // middleware
 
-app.get("/",(req,res)=>{
-    res.send("We ready!");
-})
-
-app.post("/api/expenses", async (req,res)=>{
-    const expense = req.body; // user data
-
-    if (!expense.name || !expense.amount || !expense.date){
-        return res.status(400).json({ success:false, message: "Please fill all fields."});
-    }
-
-    const newExpense = new Expense(expense)
-
-    try {
-        await newExpense.save();
-        res.status(201).json({ success: true, data: newExpense});
-    } catch (error){
-        console.error("Error in logging expense:", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });        
-    }
-});
-
-app.delete("/api/expenses/:id", async (req,res) => {
-    const {id} = req.params;
-    // console.log("id:", id);
-
-    try{
-        await Expense.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Expense deleted" });
-    } catch (error) {
-        // console.error("Expense not found");
-        res.status(404).json({ success: false, message: "Expense not found" });
-    }
-});
+app.use("/api/expenses",expenseRoutes);
 
 app.listen(5000, ()=>{
     connectDB();
-    console.log("We running at http://localhost:5000");
+    console.log("You're not gonna believe it but we live at http://localhost:5000");
 }); 
 
